78c212d2ced3b4c6f6809cd30dda24eb6fc947a3
Uday Kiran Reddy <ukreddy@erwin.com>
2020-06-09 11:25:48 +0000

    terrform

---
+++ terraform/windows10.tf
@@ -0,0 +1,225 @@
+variable "computer_name" {
+    type= string
+    default = "myvm3"
+  
+}
+
+
+variable "sec_resource_group_name"{
+    type = string
+    default = "security"
+}
+
+variable "location"{
+    type = string
+    default = "eastus"
+}
+
+variable "vnet_cidr_range"{
+    type = list(string)
+    default = ["10.1.0.0/16"]
+}
+
+variable "sec_subnet_prefixes"{
+    type = list(string)
+    default = ["10.1.0.0/24"]
+}
+
+variable "sec_subnet_names"{
+    type = string
+    default = "intranetwork"
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    
+    features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = var.sec_resource_group_name
+  location = var.location
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = azurerm_resource_group.myterraformgroup.name
+    address_space       = var.vnet_cidr_range
+    location            = var.location
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = var.sec_subnet_names
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = var.sec_subnet_prefixes
+}
+
+# Create public IPs
+
+
+resource "azurerm_public_ip" "myterraformpublicip2" {
+    name                         = "${var.computer_name}NICPublicIP2"
+    location                     = var.location
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "${var.computer_name}Network_SecurityGroup"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "RDP"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create network interface
+
+
+resource "azurerm_network_interface" "myterraformnic2" {
+    name                      = "${var.computer_name}_NIC"
+    location                  = var.location
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+        name                          = "${var.computer_name}_NICConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip2.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "example2" {
+    network_interface_id      = azurerm_network_interface.myterraformnic2.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.myterraformgroup.name
+    }
+    
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    location                    = var.location
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+# Create virtual machine
+
+
+resource "azurerm_virtual_machine" "myterraformvm2" {
+    name                  = var.computer_name
+    location              = var.location
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic2.id]
+    vm_size                  = "Standard_DS1_v2" #"Standard_DS1_v2" #https://docs.microsoft.com/en-us/azure/virtual-machines/sizes-b-series-burstable
+
+    storage_os_disk  {
+        name              = "${var.computer_name}OS_DISK"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "StandardSSD_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "MicrosoftWindowsDesktop"
+        offer     = "Windows-10"
+        sku       = "20h1-pro"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = var.computer_name
+        admin_username = "azureuser"
+        admin_password = "Notallowed1!"
+  }
+
+     os_profile_windows_config {
+    #disable_password_authentication = false
+  }
+   
+
+
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+output "ip" {
+  value = "${azurerm_public_ip.myterraformpublicip2}"
+}
