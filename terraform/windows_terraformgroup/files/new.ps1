5af92aabea48ceaf8c29cbe7914d1d29fb8a6cd1
Uday Kiran Reddy <ukreddy@erwin.com>
2020-06-12 07:16:06 +0000

    c

---
+++ terraform/windows_terraformgroup/files/new.ps1
@@ -0,0 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install googlechrome  --yes
+choco install vlc  --yes
+choco install notepadplusplus  --yes
+choco install vscode  --yes
