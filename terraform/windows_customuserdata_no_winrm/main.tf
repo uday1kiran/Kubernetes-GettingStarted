e2010368be00182fdb93a4d24f4b198ef022f1d7
Uday Kiran Reddy <ukreddy@erwin.com>
2020-06-12 11:06:22 +0000

    customdata

---
+++ terraform/windows_customuserdata_no_winrm/main.tf
@@ -0,0 +1,239 @@
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're using version 1.x, the "features" block is not allowed.
+    
+    features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "myterraformgroup" {
+  name     = var.sec_resource_group_name
+  location = var.location
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = azurerm_resource_group.myterraformgroup.name
+    address_space       = var.vnet_cidr_range
+    location            = var.location
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = var.sec_subnet_names
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = var.sec_subnet_prefixes
+}
+
+# Create public IPs
+
+
+resource "azurerm_public_ip" "myterraformpublicip2" {
+    name                         = "${var.computer_name}NICPublicIP2"
+    location                     = var.location
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "${var.computer_name}Network_SecurityGroup"
+    location            = var.location
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "HTTP"
+        priority                   = 1002
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+    security_rule {
+        name                       = "RDP"
+        priority                   = 1003
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "3389"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+	security_rule {
+        name                       = "winrm"
+        priority                   = 1004
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "5985"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+	security_rule {
+        name                       = "winrms"
+        priority                   = 1005
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "5986"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+ 
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create network interface
+
+
+resource "azurerm_network_interface" "myterraformnic2" {
+    name                      = "${var.computer_name}_NIC"
+    location                  = var.location
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+        name                          = "${var.computer_name}_NICConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip2.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+resource "azurerm_network_interface_security_group_association" "example2" {
+    network_interface_id      = azurerm_network_interface.myterraformnic2.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+
+# Generate random text for a unique storage account name
+resource "random_id" "randomId" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        resource_group = azurerm_resource_group.myterraformgroup.name
+    }
+    
+    byte_length = 8
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+    name                        = "diag${random_id.randomId.hex}"
+    resource_group_name         = azurerm_resource_group.myterraformgroup.name
+    location                    = var.location
+    account_tier                = "Standard"
+    account_replication_type    = "LRS"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+
+# Create virtual machine
+
+
+resource "azurerm_virtual_machine" "myterraformvm2" {
+    name                  = var.computer_name
+    location              = var.location
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic2.id]
+    vm_size                  = "Standard_DS1_v2" #"Standard_DS1_v2" #https://docs.microsoft.com/en-us/azure/virtual-machines/sizes-b-series-burstable
+
+    storage_os_disk  {
+        name              = "${var.computer_name}OS_DISK"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "StandardSSD_LRS"
+    }
+
+    storage_image_reference {
+        publisher = "MicrosoftWindowsDesktop"
+        offer     = "Windows-10"
+        sku       = "20h1-pro"
+        version   = "latest"
+    }
+
+    os_profile {
+        computer_name  = var.computer_name
+        admin_username = var.admin_username
+        admin_password = var.admin_password
+  }
+
+os_profile_windows_config {
+    provision_vm_agent        = true
+    winrm  {
+            protocol = "http"
+        }
+    enable_automatic_upgrades = true
+
+    # Auto-Login's required to configure WinRM
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "AutoLogon"
+      content      = "<AutoLogon><Password><Value>${var.admin_password}</Value></Password><Enabled>true</Enabled><LogonCount>1</LogonCount><Username>${var.admin_username}</Username></AutoLogon>"
+    }
+
+    # Unattend config is to enable basic auth in WinRM, required for the provisioner stage.
+    additional_unattend_config {
+      pass         = "oobeSystem"
+      component    = "Microsoft-Windows-Shell-Setup"
+      setting_name = "FirstLogonCommands"
+      content      = "${file("./files/FirstLogonCommands.xml")}"
+    }
+  }
+
+}
+
+ resource "time_sleep" "wait_30_seconds" {
+  depends_on = [azurerm_virtual_machine.myterraformvm2]
+
+  create_duration = "100s"
+} 
+    
+
+
+#https://stackoverflow.com/questions/53770872/terraform-public-ip-output-on-azure
+output "ip" {
+    value = "${azurerm_public_ip.myterraformpublicip2.*.ip_address}"
+    }
